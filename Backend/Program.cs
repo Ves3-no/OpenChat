ea2ee96ca4f5603c4214cecf01aba93c5fb2d91b
Ves3-no <heinebaranevestre@gmail.com>
2026-05-10 21:17:17 +0200

    fjerne OpenApi()

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -3,8 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontendApp",
@@ -18,10 +16,6 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
